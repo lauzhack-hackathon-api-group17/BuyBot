dd23794dd12e9ea8602fe79c4b64722e3bd73289
unusedusername01 <haris.aouini@epfl.ch>
2025-05-16 22:23:44 +0200

    Added context csv for layer 1 LLM output formating

---
+++ constants.py
@@ -0,0 +1,4 @@
+DATABASE_FORMAT = """
+                    Brand,Model,Category,Display,CPU,RAM,Storage,GPU,OS,Weight,Price
+                    Apple,MacBook Pro,Ultrabook,"13.3"", IPS Retina 2560x1600","Intel Core i5 2.3GHz",8GB,"128GB SSD","Intel Iris Plus Graphics 640",macOS,1.37kg,$36
+                  """ # Insert an example of the database format here
