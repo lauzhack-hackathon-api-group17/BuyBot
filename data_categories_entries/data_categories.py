c0823fbff17634a36193dc2446715b95edcb9265
IliasSokolisGD <iliassokb@gmail.com>
2025-05-16 23:58:16 +0200

    Made database and categories interface

---
+++ data_categories_entries/data_categories.py
@@ -0,0 +1,24 @@
+from enum import Enum
+
+class Categories(Enum):
+    LAPTOPS = (["Brand", "Model", "Category", "Display", "CPU", "RAM", "Storage", "GPU", "OS", "Weight", "Price", "Link"],
+               ["TEXT", "TEXT", "TEXT","TEXT", "TEXT", "TEXT", "TEXT", "TEXT", "TEXT","TEXT", "TEXT", "TEXT"])
+
+
+    def __init__(self, header: list, type: list):
+        self.header = header
+        self.type = type
+
+    def database_name_for_category(self):
+        return f"{self.name}.db"
+
+    def string_to_create_database(self):
+        stringToReturn = "("
+        length_header = len(self.header)
+        for i in range(length_header - 1):
+            stringToReturn += f"{self.header[i]} {self.type[i]},"
+        #append the last one
+        stringToReturn += f"{self.header[length_header -1]} {self.type[length_header -1]})"
+
+        return stringToReturn
+
--- sql_database/sql_database_interface.py
+++ sql_database/sql_database_interface.py
@@ -1,14 +1,50 @@
 import sqlite3
+import os
+from unicodedata import category
 
-LAPTOPS_NAME = "laptops.db"
+from FinanceBot.data_categories_entries.data_categories import Categories
+
+
+def create_database_if_it_does_not_exist(category: Categories):
+    file_name = category.database_name_for_category()
+    if not os.path.exists(file_name):
+        conn = sqlite3.connect(category.database_name_for_category())
+        c = conn.cursor()
+        c.execute(f"CREATE TABLE {category.name} {category.string_to_create_database()}")
+        c.close()
+
 
 class sql_database_interface:
-
-    def __init__(self, ):
-        pass
-
-    def parse_line(self, map_entry_to_value):
+    def __init__(self, category: Categories):
         """
-        :param map_entry_to_value: the map that contains the
-        :return:
+        Init the database interface with the category you want
+        :param category: the category we want to open for the database
         """
+        self.category = category
+        create_database_if_it_does_not_exist(category)
+        self.connection = sqlite3.connect(category.database_name_for_category())
+        self.c = self.connection.cursor()
+
+    def parse_line(self, tuple_values: tuple):
+        """
+        Parse the tuple line. The tuple has to have length equal to the number of elements one each row of the category
+        :param tuple_values: the tuple values associated with the category of this database
+        """
+        assert len(tuple_values) == len(self.category.header)
+        length_tuple = len(tuple_values)
+        entry_to_add = "("
+        for i in range(length_tuple - 1):
+            entry_to_add += f"'{tuple_values[i]}',"
+        #add the last one
+        entry_to_add += f"'{tuple_values[length_tuple -1]}')"
+
+        self.c.execute(f"INSERT INTO {self.category.name} VALUES {entry_to_add}")
+        self.connection.commit()
+
+
+
+    def close_connection(self):
+        """
+        Close the connection with the database
+        """
+        self.c.close()
